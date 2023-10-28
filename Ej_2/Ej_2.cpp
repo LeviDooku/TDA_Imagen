c8abe3b353f84651adf764e8602367e94c8faee3
LeviDooku <pedrovelascosantana@gmail.com>
2023-10-28 17:24:28 +0200

    Subido Ej_2 y 4

---
+++ Ej_2/Ej_2.cpp
@@ -0,0 +1,139 @@
+//Ej_2
+
+/////////////////////////////
+//Pedro Velasco Santana,   //
+//2D - D2	               //
+//Práctica 2, ejercicio 2. //
+/////////////////////////////
+
+/////////////////////////////
+//         image.h         //
+//   Declaración función   //
+/////////////////////////////
+
+/**
+  * @brief Realiza un zoom x2 a una region de la imagen
+  * @param origen Ruta donde se encuentra el archivo desde el que cargar la imagen.
+  * @pre file_path debe ser una ruta válida que contenga un fichero . pgm
+  * @param destino Ruta donde se va a guardar el archivo de la imagen resultante.
+  * @param fila Especificacion de la fila de la esquina superior izquierda de la subimagen que se va a extraer.
+  * @param col Especificacion de la columna de la esquina superior izquierda de la subimagen que se va a extraer.
+  * @param lado Tamaño del lado del cuadrado.
+  * @pre El cuadrado descrito debe estar completamente incluido en la imagen original.
+  * @return Devuelve una imagen de tamaño 2*lado-1
+  */
+
+Image Zoom2X(char *origen, char *destino, int fila, int col, int lado) const;
+
+///////////////////////////////
+//         image.cpp         //
+//   Implementación función  //
+///////////////////////////////
+
+Image Image::Zoom2X(char *origen, char *destino, int fila, int col, int lado) const{
+
+    Image imagenOrigen;
+    Image imagenFinal(2*lado-1, 2*lado-1);
+    int iF=0, jF=0;
+    if (!imagenOrigen.Load(origen)){
+        cerr << "Error: La imagen no puedo leerse" << endl;
+        cerr << "Terminando la ejecucion del programa." << endl;
+        return imagenOrigen;
+    }
+
+    iF=0, jF=0;
+    for (int i=fila; i < fila+lado; i++){
+        jF=0;
+        for (int j=col; j < col+lado; j++){
+            imagenFinal.set_pixel(iF, jF, (byte) imagenOrigen.get_pixel(i, j));
+            jF+=2;
+        }
+        iF+=2;
+    }
+
+    iF=0, jF=1;
+    for (int i = fila; i < fila+lado; i++){
+        jF=1;
+        for (int j = col; j < col+lado-1; j++){
+            imagenFinal.set_pixel(iF, jF, (byte) roundf((float) (imagenOrigen.get_pixel(i, j)+imagenOrigen.get_pixel(i, j+1)) / 2));
+            jF+=2;
+        }
+        iF+=2;
+    }
+
+    iF=1, jF=0;
+    for (int i = fila; i < fila+lado-1; i++){
+        jF=0;
+        for (int j = col; j < col+lado; j++){
+            if (jF%2==0)
+                imagenFinal.set_pixel(iF, jF, (byte) roundf((float) (imagenOrigen.get_pixel(i, j)+imagenOrigen.get_pixel(i+1, j)) / 2));
+            jF++;
+            if (jF%2!=0) {
+                imagenFinal.set_pixel(iF, jF, (byte) roundf(
+                        (float) (imagenOrigen.get_pixel(i, j) + imagenOrigen.get_pixel(i+1, j) +
+                                 imagenOrigen.get_pixel(i, j+1) + imagenOrigen.get_pixel(i+1, j+1)) / 4));
+            }
+            jF++;
+        }
+        iF+=2;
+    }
+    return imagenFinal;
+}
+
+/////////////////////////////
+//      zoom.cpp           //
+//      Función main       //
+/////////////////////////////
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <image.h>
+#include <math.h>
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+    char *origen, *destino;
+    int fila, col, lado;
+    Image image;
+
+    if (argc != 6){
+        cerr << "Error: Numero de parametros incorrecto.\n";
+        cerr << "Uso: zoom <FicheroImagenOriginal><FicheroImagenDestino><filaEsquinaSupIzq><columnaEsquinaSupIzq><lado>\n";
+        exit(1);
+    }
+
+    origen = argv[1];
+    destino = argv[2];
+    fila = atoi(argv[3]),
+    col = atoi(argv[4]),
+    lado = atoi(argv[5]);
+
+    if (!image.Load(origen)){
+        cerr << "Error: La imagen no puedo leerse" << endl;
+        cerr << "Terminando la ejecucion del programa." << endl;
+        return 1;
+    }
+
+    Image new_imagen;
+    cout << origen << " " << destino << " " << fila << " " << col << " " << lado << " " << endl;
+    new_imagen = new_imagen.Zoom2X(origen, destino, fila, col, lado);
+
+    if (new_imagen.Save(destino))
+        cout << "Se ha guardado la imagen en " << destino << endl;
+    else{
+        cerr << "Error: La imagen no pudo guardarse." << endl;
+        cerr << "terminando la ejecucion del programa." << endl;
+        return 1;
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Ej_4/Ej_4.cpp
@@ -0,0 +1,119 @@
+//Ej_4
+
+/////////////////////////////
+//Pedro Velasco Santana,   //
+//2D - D2	               //
+//Práctica 2, ejercicio 4. //
+/////////////////////////////
+
+/////////////////////////////
+//         image.h         //
+//   Declaración función   //
+/////////////////////////////
+
+/**
+  * @brief Modifica el contraste de una imagen.
+  * @param in1 Umbral inferior de la imagen de entrada.
+  * @param in2 Umbral superior de la imagen de entrada.
+  * @param out1 Umbral inferior de la imagen de salida.
+  * @param out2 Umbral superior de la imagen de salida.
+  * @pre 0 <= ( in1, in2, out1, out2 ) <= 255
+  * @pre in1 < in2
+  * @pre out1 < out2
+  * @post El objeto que llama a la funcion es modificado
+  */
+
+void AdjustContrast (byte in1, byte in2, byte out1, byte out2);
+
+///////////////////////////////
+//         image.cpp         //
+//   Implementación función  //
+///////////////////////////////
+
+void Image::AdjustContrast(byte in1, byte in2, byte out1, byte out2){
+    byte valor;
+    byte cociente = (out2 - out1) / (in2 - in1);
+    float min, max, a, b;
+    for (int i=0; i<this->size(); i++) {
+        valor = this->get_pixel(i);
+        if (valor < in1) {
+            min = 0;
+            max = (float) out1;
+            a = 0;
+            b = (float) in1;
+        } else if (valor > in2) {
+            min = (float) out2;
+            max = 255;
+            a = (float) in2;
+            b = 255;
+        } else {
+            min = (float) out1;
+            max = (float) out2;
+            a = (float) in1;
+            b = (float) in2;
+        }
+        byte nValor = (byte) roundf((min + ((cociente) * ((float) valor - a))));
+        this->set_pixel(i, nValor);
+    }
+}
+
+/////////////////////////////
+//      contraste.cpp      //
+//      Función main       //
+/////////////////////////////
+
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+#include "<image.h>"
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+    char *origen, *destino;
+    byte in_1, in_2, out_1, out_2; // nombres de los ficheros
+    Image image;
+
+    // Comprobar validez de la llamada
+    if (argc != 7){
+        cerr << "Error: Numero incorrecto de parametros.\n";
+        cerr << "Uso: contraste <img entrada> <img salida> <Umbral inf entrada> <Umbral sup entrada> <Umbral inf salida> <Umbral sup salida>\n";
+        exit (1);
+    }
+
+    // Obtener argumentos
+    origen = argv[1];
+    destino = argv[2];
+    in_1  = atoi(argv[3]);
+    in_2 = atoi(argv[4]);
+    out_1  = atoi(argv[5]);
+    out_2 = atoi(argv[6]);
+
+
+    // Leer la imagen del fichero de entrada
+    if (!image.Load(origen)){
+        cerr << "Error: La imagen no puedo leerse" << endl;
+        cerr << "Terminando la ejecucion del programa." << endl;
+        return 1;
+    }
+
+    // Mostrar los parametros de la Imagen
+    cout << endl;
+    cout << "Dimensiones de la imagen" << ":" << endl;
+    cout << "   Imagen   = " << image.get_rows()  << " filas x " << image.get_cols() << " columnas " << endl;
+
+    // Contraste
+    Image new_image;
+    image.AdjustContrast(in_1, in_2, out_1, out_2);
+
+    if (image.Save(destino))
+        cout << "Se ha guardado la imagen en " << destino << endl;
+    else{
+        cerr << "Erro: La imagen no pudo guardarse." << endl;
+        cerr << "terminando la ejecucion del programa." << endl;
+        return 1;
+    }
+    return 0;
+}
